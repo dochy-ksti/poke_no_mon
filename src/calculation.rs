b5e77de0fcee2ec97bee0676a1830ce1b3357285
juzu <dochy.ksti@gmail.com>
2024-01-02 13:27:59 +0900

    unfinished

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,1 +1,6 @@
-pub fn calculation()
+pub fn calculation(level : usize, move_power : usize, atk : usize, def : usize){
+	let d = (level * 2) / 5 + 2;
+	let d = d * move_power * atk / def;
+	let d = d / 50 + 2;
+	
+}
