ecf4bb97b4a1ac065ee05145df1b7bc3940abb33
juzu <dochy.ksti@gmail.com>
2024-01-03 19:40:41 +0900

    unfinished

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -9,7 +9,7 @@ pub struct Pokemon{
 	pub def_types : DefTypes,
 	pub weight : u32,
 	
-	pub level : u8,
+	pub level : u16,
 	pub natures : Natures,
 
 	//とくせいは別枠にする
@@ -17,7 +17,20 @@ pub struct Pokemon{
 
 impl Pokemon{
 	pub fn calc_stats(&self) -> Stats{
+		let mut stats : Stats = Default::default();
 
+		let hp = (self.base.hp() * 2 + self.iv.hp() + self.ef.hp()/4) * self.level / 100 + 10 + self.level;
+		stats.values[0] = hp;
+
+		for n in 1..6{
+			let base = self.base.values[n];
+			let iv = self.iv.values[n];
+			let ef = self.ef.values[n];
+			let nature = self.natures.values[n];
+			let v = (((base * 2 + iv + ef/4) * self.level) / 100 + 5) * nature / 10;
+			stats.values[n] = v;
+		}
+		stats
 	}
 }
 
@@ -28,9 +41,10 @@ pub enum StatKind{
 	Hp, Atk, Def, SAtk, SDef, Speed
 }
 
+/// せいかく
 pub struct Natures{
 	/// 0 は使わない
-	pub values : [u8; 6]
+	pub values : [u16; 6]
 }
 
 impl Default for Natures{
@@ -53,45 +67,59 @@ impl Natures{
 	}
 }
 
+/// ステータス
+#[derive(Debug, Default)]
 pub struct Stats{
-	pub values : [u8; 6]
+	pub values : [u16; 6]
 }
 
+impl Stats{
+	pub fn hp(&self) -> u16{ self.values[0] }
+	pub fn atk(&self) -> u16{ self.values[1] }
+	pub fn def(&self) -> u16{ self.values[2] }
+	pub fn satk(&self) -> u16{ self.values[3] }
+	pub fn sdef(&self) -> u16{ self.values[4] }
+	pub fn speed(&self) -> u16{ self.values[5] }
+}
+
+/// 個体値
 pub struct IndivisualValues{
-	pub values : [u8; 6]
+	pub values : [u16; 6]
 }
 
 impl IndivisualValues{
-	pub fn hp(&self) -> u8{ self.values[0] }
-	pub fn atk(&self) -> u8{ self.values[1] }
-	pub fn def(&self) -> u8{ self.values[2] }
-	pub fn satk(&self) -> u8{ self.values[3] }
-	pub fn sdef(&self) -> u8{ self.values[4] }
-	pub fn speed(&self) -> u8{ self.values[5] }
+	pub fn hp(&self) -> u16{ self.values[0] }
+	pub fn atk(&self) -> u16{ self.values[1] }
+	pub fn def(&self) -> u16{ self.values[2] }
+	pub fn satk(&self) -> u16{ self.values[3] }
+	pub fn sdef(&self) -> u16{ self.values[4] }
+	pub fn speed(&self) -> u16{ self.values[5] }
 }
 
+/// 努力値
 pub struct EffortValues{
-	pub values : [u8; 6]
+	pub values : [u16; 6]
 }
 
 impl EffortValues{
-	pub fn hp(&self) -> u8{ self.values[0] }
-	pub fn atk(&self) -> u8{ self.values[1] }
-	pub fn def(&self) -> u8{ self.values[2] }
-	pub fn satk(&self) -> u8{ self.values[3] }
-	pub fn sdef(&self) -> u8{ self.values[4] }
-	pub fn speed(&self) -> u8{ self.values[5] }	
+	pub fn hp(&self) -> u16{ self.values[0] }
+	pub fn atk(&self) -> u16{ self.values[1] }
+	pub fn def(&self) -> u16{ self.values[2] }
+	pub fn satk(&self) -> u16{ self.values[3] }
+	pub fn sdef(&self) -> u16{ self.values[4] }
+	pub fn speed(&self) -> u16{ self.values[5] }
 }
 
+/// 種族値
 pub struct BaseStats{
-	pub values : [u8; 6]
+	pub values : [u16; 6]
 }
 
 impl BaseStats{
-	pub fn hp(&self) -> u8{ self.values[0] }
-	pub fn atk(&self) -> u8{ self.values[1] }
-	pub fn def(&self) -> u8{ self.values[2] }
-	pub fn satk(&self) -> u8{ self.values[3] }
-	pub fn sdef(&self) -> u8{ self.values[4] }
-	pub fn speed(&self) -> u8{ self.values[5] }	
+	pub fn hp(&self) -> u16{ self.values[0] }
+	pub fn atk(&self) -> u16{ self.values[1] }
+	pub fn def(&self) -> u16{ self.values[2] }
+	pub fn satk(&self) -> u16{ self.values[3] }
+	pub fn sdef(&self) -> u16{ self.values[4] }
+	pub fn speed(&self) -> u16{ self.values[5] }
 }
