ca2263c2eb565fdbbc3cede9c9a60ca96c01525f
juzu <dochy.ksti@gmail.com>
2024-01-08 12:40:25 +0900

    unfinished

--- src/appliers/applier.rs
+++ src/appliers/applier.rs
@@ -1,32 +1,54 @@
-use super::{power_appliers::PowerAppliers, atk_appliers::AtkAppliers, def_appliers::DefAppliers, damage_appliers::DamageAppliers, speed_appliers::SpeedAppliers};
-
+use super::{
+    atk_appliers::AtkAppliers, damage_appliers::DamageAppliers, def_appliers::DefAppliers,
+    power_appliers::PowerAppliers, speed_appliers::SpeedAppliers,
+};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
-pub enum ApplierType{
-	Power, Atk, Def, Damage, Speed
+pub enum ApplierType {
+    Power,
+    Atk,
+    Def,
+    Damage,
+    Speed,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Applier{
-	pub t : ApplierType,
-	pub num : u16,
+pub struct Applier {
+    pub t: ApplierType,
+    pub num: u16,
 }
 
-impl Applier{
-	pub fn power(app : PowerAppliers) -> Applier{
-		Applier{ t : ApplierType::Power, num : app as u16 }
-	}
-	pub fn atk(app : AtkAppliers) -> Applier{
-		Applier{ t : ApplierType::Atk, num : app as u16 }
-	}
-	pub fn def(app : DefAppliers) -> Applier{
-		Applier{ t : ApplierType::Def, num : app as u16 }
-	}
-	pub fn damage(app : DamageAppliers) -> Applier{
-		Applier{ t : ApplierType::Damage, num : app as u16 }
-	}
-	pub fn speed(app : SpeedAppliers) -> Applier{
-		Applier{ t : ApplierType::Speed, num : app as u16 }
-	}
+impl Applier {
+    pub fn power(app: PowerAppliers) -> Applier {
+        todo!("applier にも temporary（ターン数で終わる) permanent(永続) interim(交代まで続く)の３つがあるはず。");
+        Applier {
+            t: ApplierType::Power,
+            num: app as u16,
+        }
+    }
+    pub fn atk(app: AtkAppliers) -> Applier {
+        Applier {
+            t: ApplierType::Atk,
+            num: app as u16,
+        }
+    }
+    pub fn def(app: DefAppliers) -> Applier {
+        Applier {
+            t: ApplierType::Def,
+            num: app as u16,
+        }
+    }
+    pub fn damage(app: DamageAppliers) -> Applier {
+        Applier {
+            t: ApplierType::Damage,
+            num: app as u16,
+        }
+    }
+    pub fn speed(app: SpeedAppliers) -> Applier {
+        Applier {
+            t: ApplierType::Speed,
+            num: app as u16,
+        }
+    }
 }
