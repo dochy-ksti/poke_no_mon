9ff2bfa90cb91a787efa6c8b1854102622585f0b
juzu <dochy.ksti@gmail.com>
2024-02-13 13:02:41 +0900

    unfinished

--- src/moves/hits.rs
+++ src/moves/hits.rs
@@ -3,12 +3,12 @@ use super::damage_move::Options;
 #[derive(Debug)]
 pub enum Hits{
 	Count(usize),
-	Compound{ probability : usize, hit_max : usize },
-	Random{ hit_min : usize, hit_max : usize },
+	Compound{ probability : usize, hit_max : usize, simplified_hits : usize, ikasama_hits : usize },
+	Random{ hit_min : usize, hit_max : usize, simplified_hits : usize, ikasama_hits : usize },
 }
 
 impl Hits{
 	pub fn new(o : &Options) -> Hits{
-		
+
 	}
 }
